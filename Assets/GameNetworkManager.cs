5637a73ff863625d5efb99f822b2aecc2594da9c
Pijus Krisiukenas <pijuskri@gmail.com>
2024-01-26 23:33:16 +0100

    start on multi

---
+++ Assets/GameNetworkManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.Multiplayer.Playmode;
+using Unity.Netcode;
+using UnityEngine;
+
+public class GameNetworkManager : NetworkBehaviour {
+  // Start is called before the first frame update
+  public GameObject playerPrefab;
+  void Start() {
+    var mppmTag = CurrentPlayer.ReadOnlyTags().First();
+    Debug.Log(mppmTag);
+    var networkManager = NetworkManager.Singleton;
+    
+    if (mppmTag.Contains("Server")) {
+      networkManager.StartServer();
+    }
+    else if (mppmTag.Contains("Host")) {
+      networkManager.StartHost();
+    }
+    else if (mppmTag.Contains("Client")) {
+      networkManager.StartClient();
+    }
+    
+  }
+  
+  public override void OnNetworkSpawn()
+  {
+    /*
+    if (networkManager.IsServer || networkManager.IsHost) {
+      
+    }*/
+    /*
+    if (IsServer)
+    {
+      SpawnPlayers(NetworkManager.Singleton.LocalClientId);
+      //NetworkManager.ConnectionApprovalCallback = ConnectionApprovalCallback;
+    }
+    else SubmitSpawnRequestServerRpc();
+    */
+  }
+  
+  [ServerRpc(RequireOwnership = false)]
+  void SubmitSpawnRequestServerRpc(ServerRpcParams rpcParams = default) {
+    Debug.Log("got request");
+    SpawnPlayers(rpcParams.Receive.SenderClientId);
+  }
+
+  void SpawnPlayers(ulong newClientId) {
+    GameObject go = (GameObject)Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+    go.GetComponent<NetworkObject>().SpawnAsPlayerObject(newClientId, true);
+  }
+
+
+  // Update is called once per frame
+  void Update() {
+  }
+}
